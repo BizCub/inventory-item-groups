7954abc8c1f3763cc5c6940599258ad1b2c1407f
BizCub <bizcubmine@gmail.com>
2026-04-01 20:47:03 +0200

    Updated to version 26.1.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ stonecutter.create(rootProject) {
         var suffix = if (it == "forge" && sc.eval(version, "1.16.5")) ".arch" else ""
         version("$version-$it", version).buildscript = "scripts/$it$suffix.gradle.kts"
     }
-    match("26.1", fb, fr, nf)
+    match("26.1.1", fb, fr, nf)
     match("1.21.11", fb, fr, nf)
     match("1.21.10", fb, fr, nf)
     match("1.21.8", fb, fr, nf)
