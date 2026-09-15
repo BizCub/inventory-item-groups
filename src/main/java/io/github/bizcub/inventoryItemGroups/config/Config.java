6a08af772b85c279d2e78f7e363d712ab0c25818
BizCub <bizcubmine@gmail.com>
2026-09-15 22:09:21 +0200

    Decouple Config from simple-config-lib using a self-contained Holder

--- src/main/java/io/github/bizcub/inventoryItemGroups/config/Config.java
+++ src/main/java/io/github/bizcub/inventoryItemGroups/config/Config.java
@@ -1,16 +1,22 @@
 package io.github.bizcub.inventoryItemGroups.config;
 
 import io.github.bizcub.inventoryItemGroups.Main;
-import io.github.bizcub.simpleConfigLib.autoconfig.ConfigProvider;
 
 import java.util.List;
 
 public interface Config {
     static Config get() {
-        return ConfigProvider.get(Config.class);
+        return Holder.INSTANCE;
     }
-    static void set(Config instance) {
-        ConfigProvider.set(Config.class, instance);
+
+    static void set(final Config config) {
+        if (config != null) {
+            Holder.INSTANCE = config;
+        }
+    }
+
+    class Holder {
+        private static Config INSTANCE = new Config() { };
     }
 
     default Sort sort() {
